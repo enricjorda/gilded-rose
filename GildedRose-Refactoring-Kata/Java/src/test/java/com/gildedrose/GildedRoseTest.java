37952486f400763bb8ecd9dba7ad18d9f361613e
enricjorda <enric.jorda@codurance.com>
2019-08-23 10:51:58 +0200

    emily bach style

--- GildedRose-Refactoring-Kata/Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose-Refactoring-Kata/Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,10 +10,22 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        CombinationApprovals.verifyAllCombinations(
+                this::updateQualityToString,
+                new String[]{
+                        "Regular",
+                        "Aged Brie",
+                        "Backstage passes to a TAFKAL80ETC concert",
+                        "Sulfuras, Hand of Ragnaros"},
+                new Integer[]{10,11,5,6,-5,0},
+                new Integer[]{0,50,49});
+    }
+
+    private String updateQualityToString(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return app.items[0].toString();
     }
 
 }
